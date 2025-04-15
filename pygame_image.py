aeebdac5d950b8b8d11fe28fd4775fbe3fe019ec
c0a241226f <c0a241226f@edu.teu.ac.jp>
2025-04-15 16:27:07 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -10,6 +10,7 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg") #練習1
+    bg_img2 =pg.transform.flip(bg_img, True, False)
     kk_img = pg.image.load("fig/3.png")  #練習2
     kk_img = pg.transform.flip(kk_img, True, False) #練習2後半
     kk_img = pg.transform.rotozoom(kk_img, 10, 1.0)
